1aff654d1ebc2a9bfa7cb3c24ef3bcf1a6685dd8
Nick Loadholtes <nick.loadholtes@datarobot.com>
2022-06-13 14:51:44 -0400

    Basic parsing mapping, stubbing in the output

--- projects/07/vmtranslate.py
+++ projects/07/vmtranslate.py
@@ -10,10 +10,19 @@ class Parser(object):
                 yield line
 
     def split_line(self, line):
-        return 
+        return line.strip().split(" ")
 
     def translate_operation(self, tokens):
-        pass
+        if not tokens:
+            raise Exception("No tokens suppled")
+        if tokens[0] in ["//", "\n", ""]:
+            return None
+        operation = operation_mappings.get(tokens[0])
+        if operation:
+            for arg in tokens[1:]:
+                operation.args.append(arg)
+        print(f"tokens: {tokens}")
+        return operation
 
     def parse(self, filename):
         print("Starting parsing")
@@ -22,26 +31,43 @@ class Parser(object):
             # print(f"  Looking at: {line}")
             tokens = self.split_line(line)
             operation = self.translate_operation(tokens)
-            self.operations.append(operation)
+            if operation:
+                self.operations.append(operation)
         print("Parsing complete")
         return self.operations
 
 
 class Operation(object):
     command = None
-    arg1 = None
-    arg2 = None
+    args = []
 
+    def __init__(self, command):
+        self.command = command
+    
+    def __repr__(self) -> str:
+        return f"{self.command} --> {self.args}"
+ 
 
 class CodeWriter(object):
-    def write_code(self, code):
+    def write_code(self, program_name, code):
         print(f"Number of ops in code: %s" % len(code))
-        raise NotImplementedError("Need to do this")
+        program = []
+        # Next steps: For each operation write out command and args in hack format
+        for op in code:
+            print(op)
+            # Translate 
+        # Write out
+        with open(program_name+".output", "w") as f:
+            f.write("\n".join(program))
 
+operation_mappings = {
+    "push": Operation("push"),
+    "add": Operation("add"),
+}
 
 if __name__ == "__main__":
     parser = Parser()
     # TODO: Read from commandline
     code = parser.parse("StackArithmetic/SimpleAdd/SimpleAdd.vm")
     cwriter = CodeWriter()
-    cwriter.write_code(code)
+    cwriter.write_code("test_program", code)
