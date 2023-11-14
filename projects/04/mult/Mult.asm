df3489122a3df8bd8a8f6cc1083d7c79cb325518
Nick Loadholtes <nick.loadholtes@datarobot.com>
2023-11-14 15:10:58 -0500

    This passes the assembler with no errors

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -1,12 +1,15 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
-
-// Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-//
-// This program only needs to handle arguments that satisfy
-// R0 >= 0, R1 >= 0, and R0*R1 < 32768.
-
-// Put your code here.
+@i
+M=1
+@sum
+M=0
+(LOOP)
+@sum
+D=M
+D=D+A
+@i
+M=M-1
+@LOOP
+0;JGT
+(END)
+@END
+0;JMP
